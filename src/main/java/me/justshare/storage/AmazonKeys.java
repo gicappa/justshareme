936112903bd9699a2f7599388409122947528882
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2010-11-17 00:33:30 +0100

    amazon S3 and SDS storage classes

---
+++ src/main/java/me/justshare/storage/AmazonKeys.java
@@ -0,0 +1,11 @@
+package me.justshare.storage;
+
+/**
+ * User: filippo@diotalevi.com
+ * Date: Nov 16, 2010
+ */
+public class AmazonKeys {
+
+    public static final String awsAccessKey = "AKIAIXGK5IRL7GO5672Q";
+    public static final String awsSecretKey = "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";
+}
