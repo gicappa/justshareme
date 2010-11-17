17a916bdfa96e9bf5ba8b189f767ae08647d3737
Gian Carlo Pace <giancarlo.pace@gmail.com>
2010-11-17 07:44:20 +0100

    Adding FileUploadController and SpacesController

---
+++ src/main/java/me/justshare/webapp/FileUploadController.java
@@ -0,0 +1,35 @@
+package me.justshare.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * $Id$
+ *
+ * @author Gian Carlo Pace <giancarlo.pace@gmail.com>
+ */
+@Controller
+@RequestMapping("/api")
+public class FileUploadController {
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String handleFormUpload(@RequestParam("name") String name,
+								   @RequestParam("file") MultipartFile file) {
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+			} catch (IOException e) {
+				e.printStackTrace();  
+			}
+			System.out.println("name : " + name);
+			return "redirect:uploadSuccess";
+		} else {
+			return "redirect:uploadFailure";
+		}
+	}
+}
