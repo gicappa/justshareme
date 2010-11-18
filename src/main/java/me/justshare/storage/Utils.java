94c7fa1a44e4fbac51b88f0f2a17a0278efea81e
Gian Carlo Pace <giancarlo.pace@gmail.com>
2010-11-18 10:32:22 +0100

    Adding bulb images and resized images to small

--- src/main/java/me/justshare/storage/Utils.java
+++ src/main/java/me/justshare/storage/Utils.java
@@ -14,53 +14,52 @@ import java.io.InputStream;
  */
 public class Utils {
 
-    public static String guessContentType(String name) {
-        if (name == null)
-            return "description";
-        else
-        {
-            name = name.toLowerCase();
-            if (name.endsWith(".jpg") || name.endsWith(".jpeg"))
-                return "image/jpeg";
-            else if (name.endsWith(".png"))
-                return "image/x-png";
-            else if (name.endsWith(".gif"))
-                return "image/gif";
-            else
-                return "application/octet-stream";
-        }
+	public static String guessContentType(String name) {
+		if (name == null)
+			return "description";
+		else {
+			name = name.toLowerCase();
+			if (name.endsWith(".jpg") || name.endsWith(".jpeg"))
+				return "image/jpeg";
+			else if (name.endsWith(".png"))
+				return "image/x-png";
+			else if (name.endsWith(".gif"))
+				return "image/gif";
+			else
+				return "application/octet-stream";
+		}
 
-    }
+	}
 
 
-    public static InputStream scaleImageIfNecessary(InputStream is, int maxwidth) throws IOException {
-        BufferedImage sourceImage = ImageIO.read(is);
+	public static InputStream scaleImageIfNecessary(InputStream is, int maxwidth) throws IOException {
+		BufferedImage sourceImage = ImageIO.read(is);
 
-        int origWidth = sourceImage.getWidth();
+		int origWidth = sourceImage.getWidth();
 
-        if (origWidth > maxwidth)
-        {
-            float scale = (float) maxwidth / (float) origWidth;
-            int targetWidth = (int)(sourceImage.getWidth() * scale);
-            int targetHeight = (int)(sourceImage.getHeight() * scale);
+		if (!(origWidth > maxwidth))
+			return is;
 
-            BufferedImage scaledImage = new BufferedImage( targetWidth,
-                    targetHeight,
-                    BufferedImage.TYPE_INT_RGB );
-            Graphics2D g2d = scaledImage.createGraphics();
-            g2d.setRenderingHint( RenderingHints.KEY_INTERPOLATION,
-                    RenderingHints.VALUE_INTERPOLATION_BILINEAR );
+		float scale = (float) maxwidth / (float) origWidth;
+		int targetWidth = (int) (sourceImage.getWidth() * scale);
+		int targetHeight = (int) (sourceImage.getHeight() * scale);
 
-            g2d.drawImage( sourceImage, 0, 0, targetWidth, targetHeight, null );
+		return scaleTo(sourceImage, targetWidth, targetHeight);
+	}
 
-            File tempFile = File.createTempFile("justshareme", "image");
-            ImageIO.write( scaledImage, "jpeg", tempFile);
+	private static InputStream scaleTo(BufferedImage sourceImage, int targetWidth, int targetHeight) throws IOException {
+		BufferedImage scaledImage = new BufferedImage(targetWidth,
+				targetHeight,
+				BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2d = scaledImage.createGraphics();
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 
-            return new FileInputStream(tempFile);
-        }
-        else
-            return is;
+		g2d.drawImage(sourceImage, 0, 0, targetWidth, targetHeight, null);
 
-    }
+		File tempFile = File.createTempFile("justshareme", "image");
+		ImageIO.write(scaledImage, "jpeg", tempFile);
 
+		return new FileInputStream(tempFile);
+	}
 }
