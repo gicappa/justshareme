4d0a3ffc7c7fa2ce581083958e16ccca5bffa650
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2010-11-18 10:21:58 +0100

    fixed bug in content type recognition

--- src/main/java/me/justshare/storage/Utils.java
+++ src/main/java/me/justshare/storage/Utils.java
@@ -17,14 +17,19 @@ public class Utils {
     public static String guessContentType(String name) {
         if (name == null)
             return "description";
-        else if (name.endsWith(".jpg") || name.endsWith(".jpeg"))
-            return "image/jpeg";
-        else if (name.endsWith(".png"))
-            return "image/x-png";
-        else if (name.endsWith(".gif"))
-            return "image/gif";
         else
-            return "application/octet-stream";
+        {
+            name = name.toLowerCase();
+            if (name.endsWith(".jpg") || name.endsWith(".jpeg"))
+                return "image/jpeg";
+            else if (name.endsWith(".png"))
+                return "image/x-png";
+            else if (name.endsWith(".gif"))
+                return "image/gif";
+            else
+                return "application/octet-stream";
+        }
+
     }
 
 
